1041d0bb74e23b6632aafb92d23f83c1dfb307cb
Picsou06 <evanninou@gmail.com>
2023-12-10 17:28:36 +0100

    Initial commit

---
+++ src/main/java/fr/picsou/elevator/Main.java
@@ -0,0 +1,32 @@
+package fr.picsou.elevator;
+
+import fr.picsou.elevator.utils.Commands.SimpleCommand;
+import org.bukkit.craftbukkit.v1_20_R2.CraftServer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Main extends JavaPlugin {
+    private static Main instance;
+
+    @Override
+    public void onEnable(){
+        saveDefaultConfig();
+        instance = this;
+        System.out.println("[Elevator] ON");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[Elevator] OFF");
+    }
+    private void createCommand(SimpleCommand command) {
+        CraftServer server = (CraftServer) getServer();
+        server.getCommandMap().register(getName(), command);
+    }
+
+    public static Main getInstance() {
+        return instance;
+    }
+    }
+
---
+++ src/main/java/fr/picsou/elevator/components/listener/ListenerManager.java
@@ -0,0 +1,10 @@
+package fr.picsou.elevator.components.listener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ListenerManager {
+
+    public ListenerManager(JavaPlugin plugin) {
+        ///plugin.getServer().getPluginManager().registerEvents(new JoinPlayerListener(), plugin);
+    }
+}
