ced7ff3bfdf5c9f832bb355bbebc9aaa789127c2
Picsou06 <110305876+Picsou06@users.noreply.github.com>
2023-12-18 18:26:56 +0100

    Update PlayerSneakListener.java

--- src/main/java/fr/picsou/elevator/components/listener/Player/PlayerSneakListener.java
+++ src/main/java/fr/picsou/elevator/components/listener/Player/PlayerSneakListener.java
@@ -14,7 +14,7 @@ public class PlayerSneakListener implements Listener {
         Player player = event.getPlayer();
         Boolean isSneaking = player.isSneaking();
         Location blockLocationUnderPlayer = new Location(player.getLocation().getWorld(), player.getLocation().getX(), player.getLocation().getY() - 1, player.getLocation().getZ(), 0, 0);
-            if (isSneaking && blockLocationUnderPlayer.getBlock().getType() == Material.OBSIDIAN) {
+            if (isSneaking && blockLocationUnderPlayer.getBlock().getType() == Material.(Main.getInstance().getConfig().getString("bloc"))) {
             teleportPlayerToObsidianAbove(player, blockLocationUnderPlayer);
         }
     }
@@ -23,7 +23,7 @@ public class PlayerSneakListener implements Listener {
         for (int i = 1; i <= Main.getInstance().getConfig().getInt("HauteurMax"); i++) {
             Location currentLocation = new Location(blockLocationUnderPlayer.getWorld(), blockLocationUnderPlayer.getX(), blockLocationUnderPlayer.getY() - i, blockLocationUnderPlayer.getZ());
 
-            if (currentLocation.getBlock().getType() == Material.OBSIDIAN) {
+            if (currentLocation.getBlock().getType() == Material.(Main.getInstance().getConfig().getString("bloc"))) {
                 Location teleportLocation = new Location(currentLocation.getWorld(), currentLocation.getX(), currentLocation.getY() + 1, currentLocation.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch());
                 player.teleport(teleportLocation);
                 break;
